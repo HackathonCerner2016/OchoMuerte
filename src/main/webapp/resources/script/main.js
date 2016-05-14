9a889d7729a6e361da18ee6552d73d1f2f277faa
Shmiggy <a_smiggy@yahoo.com>
2016-05-14 05:15:05 +0300

    UI partial
    Cthulhu bless us !

--- src/main/webapp/resources/script/main.js
+++ src/main/webapp/resources/script/main.js
@@ -15,34 +15,77 @@ function buildSongPartArray(jsString) {
     console.log(song);                              // debug
     
     $.each(songParts, function(i, i_val) 
-    {
-        bpm = i_val.bpm;
-        noteNr = i_val.notes;
+    { 
+        $('#pageWrapper').append('<div class="songPart" id="songPart'+i+'"></div>');
+        $('#songPart'+i).append('<div class="bpmDisplay" id="bpmDisplay'+i+'">' + "BPM:" + i_val.bpm + '</div>');
+        $('#songPart'+i).append('<div class="actualPartWrapper" id="actualPartWrapper'+i+'"></div>');
+        $('#actualPartWrapper'+i).append('<div class="notesContainer" id="notesContainer'+i+'"></div>');
         
-        $('#pageWrapper').append('<div class="songPart"></div>');
-        $('#songPart').append('<div class="bpmDisplay">' + "BPM:" + value.bpm + '</div>');
-        $('#songPart').append('<div class="actualPartWrapper"></div>');
-        $('#actualPartWrapper').append('<div class="notesContainer"></div>');
+        var division = 'style = "width:'+(100/i_val.notes)+'%;"';
+        
+        $('#notesContainer'+i).append('<div class="row" id="rowG'+i+'"></div>');
+        $('#notesContainer'+i).append('<div class="row" id="rowD'+i+'"></div>');
+        $('#notesContainer'+i).append('<div class="row" id="rowA'+i+'"></div>');
+        $('#notesContainer'+i).append('<div class="row" id="rowE'+i+'"></div>');
         
         var notes = i_val.values;
         $.each(notes, function(j, j_val) 
         {
             switch(j_val.chord) 
             {
-                case 'n':
-                    code block
+                case 'G':
+                    
+                    $('#rowG'+i).append('<div class="column" '+division+' id="columnG'+i+j+'"></div>');
+                    $('#rowD'+i).append('<div class="column" '+division+' id="columnD'+i+j+'"></div>');
+                    $('#rowA'+i).append('<div class="column" '+division+' id="columnA'+i+j+'"></div>');
+                    $('#rowE'+i).append('<div class="column" '+division+' id="columnE'+i+j+'"></div>');
+                    
+                    $('#columnG'+i+j).append('<div class="note">'+j_val.position+'</div>');
+                    
                     break;
-                case n:
-                    code block
+                case 'D':
+                    
+                    $('#rowG'+i).append('<div class="column" '+division+' id="columnG'+i+j+'"></div>');
+                    $('#rowD'+i).append('<div class="column" '+division+' id="columnD'+i+j+'"></div>');
+                    $('#rowA'+i).append('<div class="column" '+division+' id="columnA'+i+j+'"></div>');
+                    $('#rowE'+i).append('<div class="column" '+division+' id="columnE'+i+j+'"></div>');
+                    
+                    $('#columnD'+i+j).append('<div class="note">'+j_val.position+'</div>');
+                    
+                    break;
+                case 'A':
+                    
+                    $('#rowG'+i).append('<div class="column" '+division+' id="columnG'+i+j+'"></div>');
+                    $('#rowD'+i).append('<div class="column" '+division+' id="columnD'+i+j+'"></div>');
+                    $('#rowA'+i).append('<div class="column" '+division+' id="columnA'+i+j+'"></div>');
+                    $('#rowE'+i).append('<div class="column" '+division+' id="columnE'+i+j+'"></div>');
+                    
+                    $('#columnA'+i+j).append('<div class="note">'+j_val.position+'</div>');
+                    
+                    break;
+                case 'E':
+                    
+                    $('#rowG'+i).append('<div class="column" '+division+' id="columnG'+i+j+'"></div>');
+                    $('#rowD'+i).append('<div class="column" '+division+' id="columnD'+i+j+'"></div>');
+                    $('#rowA'+i).append('<div class="column" '+division+' id="columnA'+i+j+'"></div>');
+                    $('#rowE'+i).append('<div class="column" '+division+' id="columnE'+i+j+'"></div>');
+                    
+                    $('#columnE'+i+j).append('<div class="note">'+j_val.position+'</div>');
+                    
                     break;
                 default:
-                    default code block
+                    
+                    $('#rowG'+i).append('<div class="column" '+division+' id="columnG'+i+j+'"></div>');
+                    $('#rowD'+i).append('<div class="column" '+division+' id="columnD'+i+j+'"></div>');
+                    $('#rowA'+i).append('<div class="column" '+division+' id="columnA'+i+j+'"></div>');
+                    $('#rowE'+i).append('<div class="column" '+division+' id="columnE'+i+j+'"></div>');
+                    
             }
             
             
             
             //j_val.chord
-            //j_val.position
+            //'+j_val.position+'
             //j_val.name
         });
         
